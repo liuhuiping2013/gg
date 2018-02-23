bbe85d028b9d042398e171b036164abf48d77be9
liuhuiping <huiping.liu@innonetwork.com>
2018-02-23 16:14:25 +0800

    add install script file

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: install.sh
+#
+#         USAGE: ./install.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: levis.liu(liuhuiping2013@gmail.com)
+#  ORGANIZATION:
+#       CREATED: 2018年02月23日 16:12
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+main()
+{
+    echo 'install successful now'
+}
+
+main
+
